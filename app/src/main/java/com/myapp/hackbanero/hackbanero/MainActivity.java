7bffe515a386b3dcd30539cff637c1b5eb903c72
Kevin Gamboa <kevin-gamboa1@hotmail.com>
2016-06-05 00:35:21 -0500

    spinner funcionales

--- app/src/main/java/com/myapp/hackbanero/hackbanero/MainActivity.java
+++ app/src/main/java/com/myapp/hackbanero/hackbanero/MainActivity.java
@@ -2,10 +2,30 @@ package com.myapp.hackbanero.hackbanero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import controller.ControladorMain;
 
 public class MainActivity extends AppCompatActivity {
+    //Array de los estados de la república
+    private String [] estados = {"Aguascalientes", "Baja California","Baja California Sur",
+                                 "Campeche", "Chiapas", "Chihuahua", "Coahuila", "Colima",
+                                 "Distrito Federal", "Durango", "Estado de México", "Guanajuato",
+                                 "Guerrero", "Hidalgo", "Jalisco", "Michoacán", "Morelos", "Nayarit",
+                                 "Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo",
+                                 "San Luis Potosí", "Sinaloa", "Sonora", "Tabasco", "Tamaulipas",
+                                 "Tlaxcala", "Veracruz", "Yucatán", "Zacatecas" };
+    private String [] sectores = {"Primario", "Secundario", "Terciario"};
+    private Spinner spinnerEstados;
+    private Spinner spinnerSectores;
+    private ControladorMain controladorMain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +35,50 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+
+        //Creacion del spinner de los estados
+        spinnerEstados = (Spinner) findViewById(R.id.spinnerEstados);
+        ArrayAdapter<String> adaptadorEstados= new ArrayAdapter<String>(this,
+                     android.R.layout.simple_spinner_dropdown_item, estados);
+        spinnerEstados.setAdapter(adaptadorEstados);
+
+        //Creación del spinner de los sectores
+        spinnerSectores = (Spinner) findViewById(R.id.spinnerSectores);
+        ArrayAdapter<String> adaptadorSectores = new ArrayAdapter<String>(this,
+                      android.R.layout.simple_spinner_dropdown_item, sectores);
+        spinnerSectores.setAdapter(adaptadorSectores);
+
+        //controladorMain = new ControladorMain(this, spinnerEstados, spinnerSectores);
     }
+
+    public void botonConsultar(View view){
+        String estadoSeleccionado = spinnerEstados.getSelectedItem().toString();
+        String sectorSeleccionado = spinnerSectores.getSelectedItem().toString();
+
+        if (estadoSeleccionado == "Yucatán") {
+            switch (sectorSeleccionado) {
+                case "Primario":
+                    Toast toast = Toast.makeText(this, "Sí funciona", Toast.LENGTH_SHORT);
+                    toast.show();
+                    break;
+
+                case "Secundario":
+                    //
+                    break;
+
+                case "Terciario":
+                    //
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            Toast toast = Toast.makeText(this, "Esta es una demo, selecciona Yucatán, por favor", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+
+
 }
