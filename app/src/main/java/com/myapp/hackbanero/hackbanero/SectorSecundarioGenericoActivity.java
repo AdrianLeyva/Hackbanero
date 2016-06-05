809e0450cb82f3ff64092c84aeae04d61df0a97b
AdrianLeyva <leyva97@live.com.mx>
2016-06-05 06:41:36 -0500

    New changes

--- app/src/main/java/com/myapp/hackbanero/hackbanero/SectorSecundarioGenericoActivity.java
+++ app/src/main/java/com/myapp/hackbanero/hackbanero/SectorSecundarioGenericoActivity.java
@@ -4,14 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class SectorSecundarioGenericoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sector_manufactura);
+        setContentView(R.layout.activity_sector_secundario_generico);
 
         String subSector = getIntent().getStringExtra("subsector");
         String añoInicial = getIntent().getStringExtra("año_inicial");
