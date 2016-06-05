9b9d542797c272982ad9c6c4ff72ffd984cd2357
AdrianLeyva <leyva97@live.com.mx>
2016-06-05 00:40:32 -0500

    SectorPrimarioActivity v2.0 (Listo)

--- app/src/main/java/com/myapp/hackbanero/hackbanero/SectorPrimarioActivity.java
+++ app/src/main/java/com/myapp/hackbanero/hackbanero/SectorPrimarioActivity.java
@@ -2,6 +2,7 @@ package com.myapp.hackbanero.hackbanero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SectorPrimarioActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SectorPrimarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sector_primario);
+
+        String estado = getIntent().getStringExtra("estado");
+        TextView textViewEstado = (TextView)findViewById(R.id.textView_nombreEstado);
+        textViewEstado.setText(estado);
     }
 }
