bcae75cf26afa635d373e35c2eb58cdd9a2bcac3
AdrianLeyva <leyva97@live.com.mx>
2016-06-04 21:40:43 -0500

    first commit

--- app/src/main/java/com/myapp/hackbanero/hackbanero/MainActivity.java
+++ app/src/main/java/com/myapp/hackbanero/hackbanero/MainActivity.java
@@ -2,14 +2,18 @@ package com.myapp.hackbanero.hackbanero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Hacer la actividad FULLSCREEN
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        //Comencemos
 
     }
 }
